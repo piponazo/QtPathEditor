465765106a925ddabb3eb678e8c77e8eb996c7a7
Luis Díaz Más <Luis Daz Ms>
2015-12-16 19:12:28 +0100

    Added buttons. Showing dialog to add a new path and inserting it to the table.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,35 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
 #include <QDebug>
 
 enum class TableColum : int
 {
-    Enabled,
-    Path,
-    Exists
+	Enabled,
+	Path,
+	Exists
 };
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
   , ui(new Ui::MainWindow)
   , m_reader(HKEY_CURRENT_USER, L"Environment", L"Path")
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 
-    m_reader.Read(m_pathList);
-    int itemIdx = 0;
+	m_reader.Read(m_pathList);
+	int itemIdx = 0;
 
-    qDebug() << "paths detected: " << m_pathList.size();
-    ui->tableWidget->setRowCount(5);
+	qDebug() << "paths detected: " << m_pathList.size();
+	ui->tableWidget->setRowCount(static_cast<int>(m_pathList.size()));
 
-    for(const auto & path : m_pathList)
-    {
-        QTableWidgetItem *newItem = new QTableWidgetItem(QString::fromWCharArray(path.c_str()));
-        ui->tableWidget->setItem(itemIdx++, static_cast<int>(TableColum::Path), newItem);
-    }
+	for(const auto & path : m_pathList)
+	{
+		QTableWidgetItem *newItem = new QTableWidgetItem(QString::fromWCharArray(path.c_str()));
+		ui->tableWidget->setItem(itemIdx++, static_cast<int>(TableColum::Path), newItem);
+	}
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
+}
+
+void MainWindow::on_buttonAddPath_clicked()
+{
+	QString dir = QFileDialog::getExistingDirectory(this, tr("Select path to add"),
+		QDir::homePath(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+	if (!dir.isEmpty())
+	{
+		int rowIndex = ui->tableWidget->rowCount();
+		ui->tableWidget->insertRow(rowIndex);
+		QTableWidgetItem *newItem = new QTableWidgetItem(dir);
+		ui->tableWidget->setItem(rowIndex, static_cast<int>(TableColum::Path), newItem);
+	}
 }
--- mainwindow.h
+++ mainwindow.h
@@ -10,14 +10,17 @@ class CPathReader;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+private slots:
+	void on_buttonAddPath_clicked();
 
 private:
-    Ui::MainWindow *ui;
-    CPathReader     m_reader;
-    StringListT     m_pathList;
+	Ui::MainWindow *ui;
+	CPathReader     m_reader;
+	StringListT     m_pathList;
 };
