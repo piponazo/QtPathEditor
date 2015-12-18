919d0f1057c2dcacf426c93091299e7ba48ee4c0
Luis Díaz Más <Luis Daz Ms>
2015-12-18 20:16:26 +0100

    Added icons for the status and the enabled columns

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -18,15 +18,30 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 	ui->setupUi(this);
 
 	m_reader.Read(m_pathList);
-	int itemIdx = 0;
+	m_pathList.push_back(L"C:\\PathInventado");
+	int itemIdxInTable = 0;
 
 	qDebug() << "paths detected: " << m_pathList.size();
 	ui->tableWidget->setRowCount(static_cast<int>(m_pathList.size()));
 
+	QIcon tick(":/icons/tick.png");
+	QIcon cross(":/icons/cross.png");
+
 	for(const auto & path : m_pathList)
 	{
-		QTableWidgetItem *newItem = new QTableWidgetItem(QString::fromWCharArray(path.c_str()));
-		ui->tableWidget->setItem(itemIdx++, static_cast<int>(TableColum::Path), newItem);
+		QString pathQt = QString::fromWCharArray(path.c_str());
+
+		QTableWidgetItem *itemEn   = new QTableWidgetItem(); // enabled
+		QTableWidgetItem *itemPath = new QTableWidgetItem(pathQt);
+		QTableWidgetItem *itemEx   = new QTableWidgetItem(); // exist
+
+		itemEn->setCheckState(Qt::Checked);
+		itemEn->setTextAlignment(Qt::AlignHCenter);
+		itemEx->setIcon(QFile::exists(pathQt) ? tick : cross);
+
+		ui->tableWidget->setItem(itemIdxInTable,   static_cast<int>(TableColum::Enabled), itemEn);
+		ui->tableWidget->setItem(itemIdxInTable,   static_cast<int>(TableColum::Path), itemPath);
+		ui->tableWidget->setItem(itemIdxInTable++, static_cast<int>(TableColum::Exists), itemEx);
 	}
 
 	ui->tableWidget->setColumnWidth(static_cast<int>(TableColum::Enabled),  50);
