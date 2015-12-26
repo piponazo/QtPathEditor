0a6064a7d8aadd9ae2a7e4893fb728ad30d4b8e2
Luis D�az M�s <ldiaz@catec.aero>
2015-12-26 10:34:27 +0100

    Adapt code to work with Qt5 instead of Qt4

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -1,29 +1,16 @@
 ###########################################
-# Configuring paths properly
-###########################################
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PROJECT_SOURCE_DIR}/cmake)
-set(INC_DIR "${EXTERNAL_DIR}/include")
-set(LIB_DIR "${EXTERNAL_DIR}/lib")
-if (NOT EXTERNAL_DIR)
-    message(FATAL_ERROR "EXTERNAL_DIR variable is not set")
-endif()
-list(APPEND CMAKE_PREFIX_PATH "${EXTERNAL_DIR}")
-
-###########################################
 # Setup Qt
 ###########################################
 
-find_program(QT_QMAKE_EXECUTABLE NAMES qmake-qt4)
-find_package(Qt4 COMPONENTS QtCore QtGui REQUIRED)
-#include_directories(SYSTEM ${QT_INCLUDE_DIR} ${QT_QTCORE_INCLUDE_DIR})
+find_package(Qt5Core    REQUIRED)
+find_package(Qt5Widgets REQUIRED)
 
 ###########################################
 # Setup Google Test Framework
 ###########################################
 
-find_package(GTest REQUIRED)
-
-#add_definitions(-DGTEST_LINKED_AS_SHARED_LIBRARY=1)
+find_package(GTest      REQUIRED)
+add_definitions(-DGTEST_LINKED_AS_SHARED_LIBRARY=1)
 
 # \note This stack overflow suggests that we might have different compiler
 # options between googlemock and our code. If we want gtest with pthread
