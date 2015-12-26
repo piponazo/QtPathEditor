d7a6a48857fd367dab5d7b69fcf162615df0f34a
Luis D�az M�s <ldiaz@catec.aero>
2015-12-26 11:08:35 +0100

    Deleting entries with "Delete" key

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QShortcut>
+#include <QHeaderView>
 #include <QFileDialog>
 #include <QDesktopServices>
 #include <QUrl>
@@ -8,207 +10,221 @@
 
 namespace
 {
-	enum TableColum
-	{
-		COL_ENABLED,
-		COL_PATH,
-		COL_EXISTS
-	};
+    enum TableColum
+    {
+        COL_ENABLED,
+        COL_PATH,
+        COL_EXISTS
+    };
 
 }
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
-  , ui(new Ui::MainWindow)
-  , m_reader(HKEY_CURRENT_USER)
-  , m_config("QtPathEditor")
+    , ui(new Ui::MainWindow)
+    , m_reader(HKEY_CURRENT_USER)
+    , m_config("QtPathEditor")
 {
-	ui->setupUi(this);
-	getPaths();
+    ui->setupUi(this);
+    getPaths();
 
-	ui->tableWidget->setRowCount(m_paths.size());
+    ui->tableWidget->setRowCount(m_paths.size());
 
-	/// \todo use the indexes
-	for (int i = 0; i < m_paths.size(); ++i)
-	{
-		addPathToTable(m_paths[i], m_statuses[i], i);
-	}
+    /// \todo use the indexes
+    for(int i = 0; i < m_paths.size(); ++i)
+    {
+        addPathToTable(m_paths[i], m_statuses[i], i);
+    }
 
-	setupVisualAspect();
-	makeConnections();
+    setupVisualAspect();
+    makeConnections();
+    assignShortcuts();
 }
 
 void MainWindow::addPathToTable(const QString &path, const bool enabled, const int row)
 {
-	QTableWidgetItem *itemEn   = new QTableWidgetItem(); // enabled
-	QTableWidgetItem *itemPath = new QTableWidgetItem(path);
-	QTableWidgetItem *itemEx   = new QTableWidgetItem(); // exist
-	itemEn->setFlags(Qt::NoItemFlags | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
-	itemEx->setFlags(Qt::NoItemFlags | Qt::ItemIsEnabled);
+    QTableWidgetItem *itemEn   = new QTableWidgetItem(); // enabled
+    QTableWidgetItem *itemPath = new QTableWidgetItem(path);
+    QTableWidgetItem *itemEx   = new QTableWidgetItem(); // exist
+    itemEn->setFlags(Qt::NoItemFlags | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
+    itemEx->setFlags(Qt::NoItemFlags | Qt::ItemIsEnabled);
 
-	itemEn->setCheckState(enabled ? Qt::Checked : Qt::Unchecked);
-	itemEn->setTextAlignment(Qt::AlignHCenter);
-	itemEx->setIcon(QFile::exists(path) ? QIcon(":/icons/tick.png") : QIcon(":/icons/cross.png"));
+    itemEn->setCheckState(enabled ? Qt::Checked : Qt::Unchecked);
+    itemEn->setTextAlignment(Qt::AlignHCenter);
+    itemEx->setIcon(QFile::exists(path) ? QIcon(":/icons/tick.png") : QIcon(":/icons/cross.png"));
 
-	ui->tableWidget->setItem(row, COL_ENABLED,  itemEn);
-	ui->tableWidget->setItem(row, COL_PATH,     itemPath);
-	ui->tableWidget->setItem(row, COL_EXISTS,   itemEx);
+    ui->tableWidget->setItem(row, COL_ENABLED,  itemEn);
+    ui->tableWidget->setItem(row, COL_PATH,     itemPath);
+    ui->tableWidget->setItem(row, COL_EXISTS,   itemEx);
 }
 
 MainWindow::~MainWindow()
 {
-	on_buttonSave_clicked();
-	delete ui;
+    on_buttonSave_clicked();
+    delete ui;
 }
 
 void MainWindow::getPaths()
 {
-	StringListT  listFromRegistry;
-	m_reader.Read(listFromRegistry);
+    StringListT  listFromRegistry;
+    m_reader.Read(listFromRegistry);
 
-	// Read from the registry (all must be enabled)
-	for (int i = 0; i < static_cast<int>(listFromRegistry.size()); ++i)
-	{
-		m_paths     << QString::fromWCharArray(listFromRegistry[i].c_str());
-		m_statuses  << true;
-		m_indexes   << i;
-	}
+    // Read from the registry (all must be enabled)
+    for(int i = 0; i < static_cast<int>(listFromRegistry.size()); ++i)
+    {
+        m_paths     << QString::fromWCharArray(listFromRegistry[i].c_str());
+        m_statuses  << true;
+        m_indexes   << i;
+    }
 
-	// Read from the config file (it can override the status of the previous paths)
-	const QStringList paths    = m_config.getPaths();
-	const QBitArray   statuses = m_config.getStatus();
-	Q_ASSERT (paths.size() == statuses.size());
+    // Read from the config file (it can override the status of the previous paths)
+    const QStringList paths    = m_config.getPaths();
+    const QBitArray   statuses = m_config.getStatus();
+    Q_ASSERT(paths.size() == statuses.size());
 
-	for (int i = 0; i < paths.size(); ++i)
-	{
-		const int idx = m_paths.indexOf(paths[i]);
-		if (idx != -1) // Already in the list -> update the status
-		{
-			m_statuses[i] = statuses[i];
-		}
-		else
-		{
-			m_paths     << m_paths[i];
-			m_statuses  << statuses[i];
-			m_indexes   << (m_paths.size() - 1);
-		}
-	}
+    for(int i = 0; i < paths.size(); ++i)
+    {
+        const int idx = m_paths.indexOf(paths[i]);
+
+        if(idx != -1)  // Already in the list -> update the status
+        {
+            m_statuses[i] = statuses[i];
+        }
+        else
+        {
+            m_paths     << m_paths[i];
+            m_statuses  << statuses[i];
+            m_indexes   << (m_paths.size() - 1);
+        }
+    }
 }
 
 void MainWindow::on_buttonSave_clicked()
 {
-	saveConfigFile();
-	saveRegistry();
+    saveConfigFile();
+    saveRegistry();
 }
 
 void MainWindow::itemPressed(QTableWidgetItem *item)
 {
-	switch (item->column())
-	{
-	case COL_ENABLED :
-	{
-		const int idx = m_indexes[item->row()];
-		const bool checked = item->checkState() == Qt::Checked;
-		m_statuses[idx] = checked;
-		qDebug() << "Path changed its status to: " << checked;
-		break;
-	}
-	default:
-	{
-		qDebug() << "itemPressed. Action not implemented with colum: " << item->column();
-	}
-	}
+    switch(item->column())
+    {
+        case COL_ENABLED :
+        {
+            const int idx = m_indexes[item->row()];
+            const bool checked = item->checkState() == Qt::Checked;
+            m_statuses[idx] = checked;
+            qDebug() << "Path changed its status to: " << checked;
+            break;
+        }
+
+        default:
+        {
+            qDebug() << "itemPressed. Action not implemented with colum: " << item->column();
+        }
+    }
 }
 
 void MainWindow::saveRegistry()
 {
-	StringListT listToRegistry;
+    StringListT listToRegistry;
 
-	for (int i = 0; i < m_paths.size(); ++i)
-	{
-		const int idx = m_indexes[i];
-		if (m_statuses[idx])
-		{
-			listToRegistry.push_back(m_paths[idx].toStdWString());
-		}
-	}
+    for(int i = 0; i < m_paths.size(); ++i)
+    {
+        const int idx = m_indexes[i];
 
-	m_reader.Write(listToRegistry);
+        if(m_statuses[idx])
+        {
+            listToRegistry.push_back(m_paths[idx].toStdWString());
+        }
+    }
+
+    m_reader.Write(listToRegistry);
 }
 
 void MainWindow::saveConfigFile()
 {
-	m_config.setPaths(m_paths);
-	m_config.setOrder(m_indexes);
+    m_config.setPaths(m_paths);
+    m_config.setOrder(m_indexes);
 
-	QBitArray array (m_statuses.size());
-	for (int i = 0; i < m_statuses.size(); ++i)
-	{
-		array.setBit(i, m_statuses[i]);
-	}
-	m_config.setStatus(array);
+    QBitArray array(m_statuses.size());
+
+    for(int i = 0; i < m_statuses.size(); ++i)
+    {
+        array.setBit(i, m_statuses[i]);
+    }
+
+    m_config.setStatus(array);
+}
+
+void MainWindow::assignShortcuts()
+{
+    qDebug() << "Key Delete pressed";
+    QShortcut *shortcut = new QShortcut(QKeySequence("Del"), this);
+    connect(shortcut, SIGNAL(activated()), this, SLOT(on_buttonDeletePath_clicked()));
 }
 
 void MainWindow::on_buttonBrowse_clicked()
 {
-	const int idx = m_indexes[ui->tableWidget->currentRow()];
-	if (m_statuses[idx])
-	{
-		QDesktopServices::openUrl(m_paths[idx]);
-	}
-	else
-	{
-		qDebug() << "Non existing path will be not opened";
-	}
+    const int idx = m_indexes[ui->tableWidget->currentRow()];
+
+    if(m_statuses[idx])
+    {
+        QDesktopServices::openUrl(m_paths[idx]);
+    }
+    else
+    {
+        qDebug() << "Non existing path will be not opened";
+    }
 }
 
 void MainWindow::sectionMoved(int logicalIndex, int oldVisualIndex, int newVisualIndex)
 {
-	qDebug() << "Section moved: " << logicalIndex << oldVisualIndex << newVisualIndex;
+    qDebug() << "Section moved: " << logicalIndex << oldVisualIndex << newVisualIndex;
 }
 
 
 void MainWindow::setupVisualAspect()
 {
-	ui->tableWidget->verticalHeader()->setMovable(true);
-	ui->tableWidget->verticalHeader()->setDragEnabled(true);
-	ui->tableWidget->verticalHeader()->setDragDropMode(QAbstractItemView::InternalMove);
+    ui->tableWidget->verticalHeader()->setSectionsMovable(true);
+    ui->tableWidget->verticalHeader()->setDragEnabled(true);
+    ui->tableWidget->verticalHeader()->setDragDropMode(QAbstractItemView::InternalMove);
 
-	ui->tableWidget->setColumnWidth(COL_ENABLED,  50);
-	ui->tableWidget->resizeColumnToContents(COL_PATH);
-	ui->tableWidget->setColumnWidth(COL_EXISTS,   50);
+    ui->tableWidget->setColumnWidth(COL_ENABLED,  50);
+    ui->tableWidget->resizeColumnToContents(COL_PATH);
+    ui->tableWidget->setColumnWidth(COL_EXISTS,   50);
 }
 
 void MainWindow::makeConnections()
 {
-	connect(ui->tableWidget, SIGNAL(itemChanged(QTableWidgetItem *)),
-			this, SLOT(itemPressed(QTableWidgetItem *)));
-	connect(ui->tableWidget->verticalHeader(), SIGNAL(sectionMoved(int,int,int)),
-			this, SLOT(sectionMoved(int,int,int)));
+    connect(ui->tableWidget, SIGNAL(itemChanged(QTableWidgetItem *)),
+            this, SLOT(itemPressed(QTableWidgetItem *)));
+    connect(ui->tableWidget->verticalHeader(), SIGNAL(sectionMoved(int, int, int)),
+            this, SLOT(sectionMoved(int, int, int)));
 }
 
 void MainWindow::on_buttonAddPath_clicked()
 {
-	QString dir = QFileDialog::getExistingDirectory(this, tr("Select path to add"),
-		QDir::homePath(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Select path to add"),
+                  QDir::homePath(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
 
-	if (!dir.isEmpty())
-	{
-		/// \todo check if the path exists?
-		m_paths     << dir;
-		m_statuses  << true;
-		m_indexes   << (m_paths.size() - 1);
-		const int row = ui->tableWidget->rowCount();
-		ui->tableWidget->insertRow(row);
-		addPathToTable(dir, true, row);
-	}
+    if(!dir.isEmpty())
+    {
+        /// \todo check if the path exists?
+        m_paths     << dir;
+        m_statuses  << true;
+        m_indexes   << (m_paths.size() - 1);
+        const int row = ui->tableWidget->rowCount();
+        ui->tableWidget->insertRow(row);
+        addPathToTable(dir, true, row);
+    }
 }
 
 void MainWindow::on_buttonDeletePath_clicked()
 {
-	const int dataIndex = m_indexes[ui->tableWidget->currentRow()];
-	m_paths.erase(m_paths.begin() + m_indexes[dataIndex]);
-	m_statuses.erase(m_statuses.begin() + m_indexes[dataIndex]);
-	m_indexes.erase(m_indexes.begin() + dataIndex);
-	ui->tableWidget->removeRow(ui->tableWidget->currentRow());
+    const int dataIndex = m_indexes[ui->tableWidget->currentRow()];
+    m_paths.erase(m_paths.begin() + m_indexes[dataIndex]);
+    m_statuses.erase(m_statuses.begin() + m_indexes[dataIndex]);
+    m_indexes.erase(m_indexes.begin() + dataIndex);
+    ui->tableWidget->removeRow(ui->tableWidget->currentRow());
 
 }
--- app/mainwindow.h
+++ app/mainwindow.h
@@ -14,45 +14,46 @@ class QTableWidgetItem;
 
 class MainWindow : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit MainWindow(QWidget *parent = 0);
-	~MainWindow();
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
 
-	/// Get paths from the registry and from the user configuration
-	void getPaths();
+    /// Get paths from the registry and from the user configuration
+    void getPaths();
 
 private slots:
-	/// Open the selected path in a new Windows Explorer
-	void on_buttonBrowse_clicked();
+    /// Open the selected path in a new Windows Explorer
+    void on_buttonBrowse_clicked();
 
-	/// Add a new path to the list of paths
-	void on_buttonAddPath_clicked();
+    /// Add a new path to the list of paths
+    void on_buttonAddPath_clicked();
 
-	/// Remove an existing path from the list of paths
-	void on_buttonDeletePath_clicked();
+    /// Remove an existing path from the list of paths
+    void on_buttonDeletePath_clicked();
 
-	void on_buttonSave_clicked();
+    void on_buttonSave_clicked();
 
-	void itemPressed(QTableWidgetItem *item);
+    void itemPressed(QTableWidgetItem *item);
 
-	void sectionMoved(int logicalIndex, int oldVisualIndex, int newVisualIndex);
+    void sectionMoved(int logicalIndex, int oldVisualIndex, int newVisualIndex);
 
 
 private:
-	void addPathToTable(const QString &path, const bool enabled, const int row);
-	void setupVisualAspect();
-	void makeConnections();
-	void saveRegistry();
-	void saveConfigFile();
+    void addPathToTable(const QString &path, const bool enabled, const int row);
+    void setupVisualAspect();
+    void makeConnections();
+    void saveRegistry();
+    void saveConfigFile();
+    void assignShortcuts();
 
-	Ui::MainWindow *ui;
+    Ui::MainWindow *ui;
 
-	CPathReader     m_reader;
-	ConfigFile      m_config;
+    CPathReader     m_reader;
+    ConfigFile      m_config;
 
-	QList<QString>  m_paths;
-	QList<bool>     m_statuses;
-	QList<int>      m_indexes;
+    QList<QString>  m_paths;
+    QList<bool>     m_statuses;
+    QList<int>      m_indexes;
 };
